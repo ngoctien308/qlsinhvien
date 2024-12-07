89c336116c20f31c97142e2affe39b8644541d6c
nguyenhoang123213 <nguyendinhhoang392@gmail.com>
2024-12-07 15:27:53 +0700

    crud-sv

---
+++ student_info/edit_sv.php
@@ -0,0 +1,78 @@
+<?php require_once "pdo.php" ;
+    $id = $_GET["id"];
+    $sql = "select * from sinhvien where id = ?";
+    $data= pdo_query_one($sql,$id);
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $ten = $_POST["ten"];
+        $ma_sv = $_POST["ma_sv"];
+        $ngay_sinh = $_POST["ngaysinh"];
+        $sdt = $_POST["sdt"];
+        $email = $_POST["email"];
+        $diachi = $_POST["diachi"];
+        $gioi_tinh = $_POST["gioi_tinh"];
+    
+        // Câu lệnh SQL cập nhật
+        $sql_update = "UPDATE sinhvien SET ten = ?, ma_sv = ?, ngay_sinh = ?, sdt = ?, email = ?, diachi = ?, gioi_tinh = ? WHERE id = ?";
+        pdo_execute($sql_update, $ten, $ma_sv, $ngay_sinh, $sdt, $email, $diachi, $gioi_tinh, $id);
+    
+        // Chuyển hướng sau khi cập nhật thành công
+        header("Location: index.php");
+        exit;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="container mt-5">
+    <h2 class="text-center mb-4">Sửa thông tin sinh viên</h2>
+    <form action="edit_sv.php?id=<?=$id?>" method="POST">
+        <div class="form-group mb-3">
+            <label for="name1">Tên sinh viên</label>
+            <input type="text" id="name1" name="ten" value="<?=$data["ten"]?>" class="form-control" placeholder="Nhập tên sinh viên">
+        </div>
+        <div class="form-group mb-3">
+            <label for="name2">Mã sinh viên</label>
+            <input type="text" id="name2" name="ma_sv" value="<?=$data["ma_sv"]?>" class="form-control" placeholder="Nhập lớp">
+        </div>
+        <div class="form-group mb-3">
+            <label for="name3">Ngày sinh</label>
+            <input type="date" id="name3" name="ngaysinh" value="<?=$data["ngay_sinh"]?>" class="form-control" placeholder="Nhập ngày sinh">
+        </div>
+        <div class="form-group mb-3">
+            <label for="name4">Số điện thoại</label>
+            <input type="text" id="name4" name="sdt"  value="<?=$data["sdt"]?>"class="form-control" placeholder="Nhập số điện thoại">
+        </div>
+        <div class="form-group mb-3">
+            <label for="name5">Email</label>
+            <input type="email" id="name5" name="email" value="<?=$data["email"]?>" class="form-control" placeholder="Nhập email">
+        </div>
+        <div class="form-group mb-3">
+            <label for="name6">Địa chỉ</label>
+            <input type="text" id="name6" name="diachi" value="<?=$data["diachi"]?>" class="form-control" placeholder="Nhập địa chỉ">
+        </div>
+        <div class="form-group mb-3">
+            <label for="name6">Giới tính</label>
+             <select name="" class="form-select" id="">
+                <option value="">Giới tính</option>
+                <option <?=$data["gioi_tinh"]==1 ? "selected" :""?> value="1">Giới tính nam</option>
+                <option <?=$data["gioi_tinh"]==2 ? "selected" :""?> value="2">Giới tính nữ</option>
+             </select>
+        </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary">Lưu thông tin</button>
+            <button type="reset" class="btn btn-secondary">Làm mới</button>
+        </div>
+    </form>
+</div>
+
+  
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
--- student_info/index.php
+++ student_info/index.php
@@ -1,40 +1,53 @@
-<?php 
-include('../includes/head.php');
-include('../includes/header.php');
-
-$result = $conn->query('select * from sinhvien');
+<?php require_once 'pdo.php';
+    $sql = "Select * from sinhvien where 1 ";
+    $data = pdo_query($sql);
+  
 ?>
-
-<div class='container'>
-    <h1 class='mt-2'>Sinh viên</h1>
-    <button type="button" class="btn btn-info mb-2">Thêm mới sinh viên</button>
-    <table class="table table-hover">
-    <thead>
-        <tr class='table-active'>
-        <th scope="col">ID</th>
-        <th scope="col">Tên sinh viên</th>
-        <th scope="col">Lớp</th>
-        <th scope="col">Địa chỉ</th>
-        <th scope="col">SDT</th>
-        <th scope="col" colspan="2">Tùy chọn</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    echo '<tr>
-                        <th scope="row">'. $row['id'] .'</th>
-                        <td>'. $row['ten'] .'</td>
-                        <td>'. $row['lop'] .'</td>
-                        <td>'. $row['diachi'] .'</td>
-                        <td>'. $row['sdt'] .'</td>
-                        <td><button type="button" class="btn btn-warning">Sửa</button></td>
-                        <td><button type="button" class="btn btn-danger">Xóa</button></td>
-                        </tr>';
-                }
-            }
-        ?>
-    </tbody>
-    </table>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="text-center">
+        <h2>Quản lý thông tin sinh viên</h2>
+        </div>
+        <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Tên</th>
+      <th scope="col">Mã sinh viên</th>
+      <th scope="col">Ngày sinh</th>
+      <th scope="col">Giới tính</th>
+      <th scope="col">Quê quán</th>
+      <th scope="col">Email</th>
+      <th scope="col">SDT</th>
+      <th scope="col">Thao tác</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($data as $index => $item) :?>
+    <tr>
+      <th scope="row"><?=$index + 1?></th>
+      <td><?=$item['ten']?></td>
+      <td><?=$item['ma_sv']?></td>
+      <td><?=$item['ngay_sinh']?></td>
+      <td><?=$item['gioi_tinh'] == 1 ? "Nam" : "Nữ"?></td>
+      <td><?=$item['diachi']?></td>
+      <td><?=$item['email']?></td>
+      <td><?=$item['sdt']?></td>
+      <td><a href="edit_sv.php?id=<?php echo $item["id"] ?>" class="btn btn-primary">Sửa</a></td>
+      
+    </tr>
+    <?php endforeach ?>
+  </tbody>
+</table>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
