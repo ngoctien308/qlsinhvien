e1ca49ee7355535945de026a526f699b3e8d9b9e
nguyenhoang123213 <nguyendinhhoang392@gmail.com>
2024-12-07 16:33:02 +0700

    update back home

--- student_info/index.php
+++ student_info/index.php
@@ -47,6 +47,8 @@
     <?php endforeach ?>
   </tbody>
 </table>
+ <a href=" http://localhost:3000/index.php " class="btn btn-secondary">Quay về trang chủ</a>
+
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
